a05692316e3e707982bab445328505a76cc8ce44
Dor.Havusha <dor.havusha.eb@renesas.com>
2025-02-16 14:44:40 +0200

    Build sort selector

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 
 // Project: Building a Video Game Discovey App
 
-import { Grid, GridItem, Show } from "@chakra-ui/react";
+import { Grid, GridItem, HStack, Show } from "@chakra-ui/react";
 import NavBar from "./components/NavBar";
 import GameGrid from "./components/GameGrid";
 import GenreList from "./components/GenreList";
@@ -9,6 +9,7 @@ import { useState } from "react";
 import { Genre } from "./hooks/useGenres";
 import PlatformSelector from "./components/PlatformSelector";
 import { Platform } from "./hooks/useGames";
+import SortSelector from "./components/SortSelector";
 
 export interface GameQuery {
   genre: Genre | null;
@@ -38,7 +39,10 @@ function App() {
         </GridItem>
       </Show>
       <GridItem area="main">
+        <HStack spacing={5} paddingLeft={2} marginBottom={5}>
         <PlatformSelector selectedPlatform={gameQuery.platform} onSelectPlatform={(platform) => setGameQuery({...gameQuery, platform})} />
+        <SortSelector />
+        </HStack>
         <GameGrid gameQuery={gameQuery} />
       </GridItem>
     </Grid>
