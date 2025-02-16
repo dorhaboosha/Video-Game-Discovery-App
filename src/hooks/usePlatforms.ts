9d12376850f570049f8123f6d56f360d0c6b1d55
Dor.Havusha <dor.havusha.eb@renesas.com>
2025-02-16 13:58:12 +0200

    Filter games by platform

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -3,7 +3,7 @@ import useData from "./useData";
 interface Platform {
     id: number;
     name: string;
-    slig: string;
+    slug: string;
 }
 
 const usePlatforms = () => useData<Platform>('/platforms/lists/parents');
