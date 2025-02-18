5dd5de13fe0deeedfe4907029225bc514c25d001
Dor.Havusha <dor.havusha.eb@renesas.com>
2025-02-18 23:24:25 +0200

    Make playstation to show

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -19,7 +19,7 @@ export interface Game {
 
 const useGames = (gameQuery: GameQuery) => 
     useData<Game>('/games',
-        { params: { genres: gameQuery.genre?.id, platforms: gameQuery.platform?.id, ordering: gameQuery.sortOrder, search: gameQuery.searchText } },
+        { params: { genres: gameQuery.genre?.id, parent_platforms: gameQuery.platform?.id, ordering: gameQuery.sortOrder, search: gameQuery.searchText } },
         [gameQuery]);
 
 export default useGames;
