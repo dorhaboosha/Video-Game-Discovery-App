63858bfbaf243dc14b01bd7c77e20861ef63e6fc
Dor Haboosha <134487550+dorhaboosha@users.noreply.github.com>
2025-03-28 14:23:18 +0300

    Refactor- simplify game query

--- src/App.tsx
+++ src/App.tsx
@@ -13,8 +13,8 @@ import SortSelector from "./components/SortSelector";
 import GameHeading from "./components/GameHeading";
 
 export interface GameQuery {
-  genre: Genre | null;
-  platform: Platform | null;
+  genreId?: number;
+  platformId?: number;
   sortOrder: string;
   searchText: string;
 }
@@ -38,7 +38,7 @@ function App() {
       </GridItem>
       <Show above="lg">
         <GridItem area="aside" paddingX={5}>
-          <GenreList selectedGenre={gameQuery.genre} onSelectGenre={(genre) => setGameQuery({...gameQuery, genre})} />
+          <GenreList selectedGenreId={gameQuery.genreId} onSelectGenre={(genre) => setGameQuery({...gameQuery, genreId: genre.id})} />
         </GridItem>
       </Show>
       <GridItem area="main">
@@ -46,7 +46,7 @@ function App() {
           <GameHeading gameQuery={gameQuery} />
           <Flex marginBottom={5}>
             <Box marginRight={5}>
-              <PlatformSelector selectedPlatform={gameQuery.platform} onSelectPlatform={(platform) => setGameQuery({...gameQuery, platform})} />
+              <PlatformSelector selectedPlatformId={gameQuery.platformId} onSelectPlatform={(platform) => setGameQuery({...gameQuery, platformId: platform.id})} />
             </Box>
             <SortSelector sortOrder={gameQuery.sortOrder} onSelectSortOrder={(sortOrder) => setGameQuery({...gameQuery, sortOrder})} />
           </Flex>
--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -5,10 +5,10 @@ import getCroppedImageUrl from '../services/image-url';
 
 interface Props {
     onSelectGenre: (genre: Genre) => void;
-    selectedGenre: Genre | null;
+    selectedGenreId?: number;
 }
 
-const GenreList = ({onSelectGenre, selectedGenre}: Props) => {
+const GenreList = ({onSelectGenre, selectedGenreId}: Props) => {
 
     const { data, isLoading, error } = useGenres();
 
@@ -28,7 +28,7 @@ const GenreList = ({onSelectGenre, selectedGenre}: Props) => {
                 <ListItem key={genre.id} paddingY='5px'>
                     <HStack>
                         <Image objectFit='cover' boxSize="32px" borderRadius={8} src={getCroppedImageUrl(genre.image_background)} />
-                        <Button whiteSpace='normal' textAlign='left' fontWeight={genre.id === selectedGenre?.id ? 'bold' : 'normal' } onClick={() => onSelectGenre(genre)} fontSize='lg' variant='link'>{genre.name}</Button>
+                        <Button whiteSpace='normal' textAlign='left' fontWeight={genre.id === selectedGenreId ? 'bold' : 'normal' } onClick={() => onSelectGenre(genre)} fontSize='lg' variant='link'>{genre.name}</Button>
                     </HStack>
                 </ListItem>)}
             </List>
