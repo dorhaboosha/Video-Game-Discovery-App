71fdf1114dfbbd6d87e6309d2130eb806ff554f9
Dor Haboosha <134487550+dorhaboosha@users.noreply.github.com>
2025-04-06 16:36:53 +0300

    Handle error + fixing project URL

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,14 +3,16 @@ import { createBrowserRouter } from "react-router-dom";
 import Layout from "./pages/Layout";
 import HomePage from "./pages/HomePage";
 import GameDetailPage from "./pages/GameDetailPage";
+import ErrorPage from "./pages/ErrorPage";
 
 const router = createBrowserRouter([
     {
-        path: '/Video-Game-Discovery-App',
+        path: '/',
         element: <Layout />,
+        errorElement: <ErrorPage />,
         children: [
             { index: true, element: <HomePage /> }, 
-            { path: 'games/:id', element: <GameDetailPage />}
+            { path: 'games/:id', element: <GameDetailPage />},
         ]
     }
 ]);
